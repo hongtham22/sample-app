0afb5401ed4cfac7172793ad2c5406b9fced663b
hongtham22 <hththam22@gmail.com>
2024-09-19 12:55:53 +0700

    Show all users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   #return the hash digest of the given string
   #cost: computational level to calculate the hash
