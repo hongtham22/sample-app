701effa5f0cb238cf39c42f73e1277aa94816450
hongtham22 <hththam22@gmail.com>
2024-09-19 10:09:17 +0700

    Update Gemfile.lock

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
-      flash[:error] = "Something went wrong"
-      render "new"
+      flash[:error] = 'Something went wrong'
+      render 'new'
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
